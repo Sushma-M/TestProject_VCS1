6ee71e474e13b63a6da6c251e0e33c413b92ad0a
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-08-28 11:54:45 +0000

    3rwewew New files added - [AdventureWorks2014,CountryInfoService,FeedService,FileService,MyJavaService,googleapis,heartRate,hrdb,fonts,material,en.json,material] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,data,lib,logos,momentLocale,ngLocale,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,pom.xml,README.md,Readme.txt,Readme.txt,README.md] Removed files - [fonts,gray-orange]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestProject_VCS1",
+  "homePage" : "Main",
+  "name" : "TestProject_VCS1",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
